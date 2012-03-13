c312d498ff942b33f52a2b5161661e1df5332b9b
Stephane Alnet <stephane@shimaore.net>
2012-03-13 13:39:45 +0100

    Added ccnq3 doc

--- make.sh
+++ make.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 (cd ../Telecoms/esl && npm run-script prepublish)
 cp ../Telecoms/esl/docs/* esl/
+
+(cd ../Telecoms/ccnq3/doc && make)
+cp -r ../Telecoms/ccnq3/doc/html/* ccnq3/
+(cd ../Telecoms/ccnq3/doc && make clean)
